a5722babde7891203284e25022f6dc3a579ae60e
jenn210 <jennifer.c.tuten@gmail.com>
2018-11-03 04:04:42 -0500

    trying to fix modal in survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,14 +4,13 @@ module.exports = function (app) {
     //Links to current list of friends 
     app.get("/api/friends", function (req, res) {
         res.json(friendData);
-        
     })
     //Push new friend data when user submits their responses.
     app.post('/api/friends', function (req, res) {
 
 //Compare user with existing friends
 //Loop through friends and calculate differences
-//Return result & display in modal
+//Return/display results
 let bestMatch = {
     name: "",
     photo: "",
@@ -22,7 +21,7 @@ let bestMatch = {
 let userInput = req.body;
 let userScores = userInput.scores;
 
-//Double for loop in order to loop through friends first, then through each friend's scores. 
+//Double for loop in order to iterate through friends first, then through each friend's scores. 
 for (let i = 0; i < friendData.length; i++) {
     let totalDifference = 0;
 
