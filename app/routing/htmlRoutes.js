6d2b7fc973f239a86d165b2001c2c5099d189a9b
jenn210 <jennifer.c.tuten@gmail.com>
2018-11-03 02:56:26 -0500

    make changes to server.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,6 +4,9 @@ module.exports = function(app) {
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     })
+    app.get("/results", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/results.html"));
+    })
     //Default route that directs to the home page.
     app.get("/", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/home.html"))
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-let path = require('path');
-//console.log((path.join(__dirname,"./app/routing/apiRoutes")))
+
+
+let cors = require('cors');
+  
 let app = express();
 
 let PORT = process.env.PORT || 9898;
@@ -9,6 +11,13 @@ let PORT = process.env.PORT || 9898;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+app.use(cors({
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'origin': '*',
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+  }));
 
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
