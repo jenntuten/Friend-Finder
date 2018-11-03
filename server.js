5aabd273f913ba8ccf83cf3ea0e7b9083bef727d
jenn210 <jennifer.c.tuten@gmail.com>
2018-11-03 18:38:39 -0500

    make changes to server

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
+
 let express = require('express');
 let bodyParser = require('body-parser');
-  
+var path = require('path')
+var serveStatic = require('serve-static')
+
 let app = express();
 
 let PORT = process.env.PORT || 9898;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
+app.use(serveStatic(path.join(__dirname, 'public')))
 
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
 
 app.listen(PORT, function() {
-    console.log("app listening on PORT: "+PORT);
+    console.log("Server listening on PORT: "+PORT);
 })
