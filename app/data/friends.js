a93885b75d65ae1b7379ab5fafd9a705cc9a8f84
jenn210 <jennifer.c.tuten@gmail.com>
2018-11-05 21:48:03 -0600

    update server.js and friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,16 +4,16 @@ let friends = [
         "name": "Michael",
         "photo": "https://i.imgur.com/4ENRXQv.png",
         "scores": [
-            5,
+            4,
             2,
             3,
-            4,
-            5,
-            1,
-            2,
-            4,
             3,
-            1
+            3,
+            2,
+            2,
+            2,
+            3,
+            4
         ]
     },
     {
@@ -48,6 +48,70 @@ let friends = [
             3
         ]
     },
+    {
+        "name": "Dwight",
+        "photo": "https://i.imgur.com/YtYHCBW.jpg",
+        "scores": [
+            4,
+            4,
+            1,
+            3,
+            3,
+            4,
+            4,
+            5,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "Kelly",
+        "photo": "https://i.imgur.com/T8jI45l.jpg",
+        "scores": [
+            4,
+            2,
+            2,
+            3,
+            1,
+            2,
+            2,
+            2,
+            4,
+            3
+        ]
+    },
+    {
+        "name": "Oscar",
+        "photo": "https://i.imgur.com/vGlwND0.jpg",
+        "scores": [
+            4,
+            4,
+            3,
+            3,
+            3,
+            3,
+            4,
+            4,
+            2,
+            4
+        ]
+    },
+    {
+        "name": "Andy",
+        "photo": "https://i.imgur.com/qPdWNWx.jpg",
+        "scores": [
+            4,
+            2,
+            2,
+            2,
+            3,
+            2,
+            3,
+            2,
+            3,
+            3
+        ]
+    }
 ]
 
 module.exports = friends;
--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 
 let express = require('express');
 let bodyParser = require('body-parser');
-var path = require('path')
-var serveStatic = require('serve-static')
 
 let app = express();
 
 let PORT = process.env.PORT || 9898;
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(serveStatic(path.join(__dirname, 'public')))
+app.use(express.static("public"));
 
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
 
-app.listen(PORT, function() {
-    console.log("Server listening on PORT: "+PORT);
+app.listen(PORT, function () {
+    console.log("Server listening on PORT: " + PORT);
 })
